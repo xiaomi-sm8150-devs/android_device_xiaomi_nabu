dfd0ac5c75f3cb6a77266a0b4c4c8a69c0897e81
Matsvei Niaverau <matvej2002@gmail.com>
2020-10-23 09:35:08 +0200

    nabu: Inherit from common tree
    Change-Id: I1e13b71d9218cdb1bd8e3ea023dfb79b1d37cb9d

---
+++ BoardConfig.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm8150-common
+include device/xiaomi/sm8150-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/nabu
+
+# Display
+TARGET_SCREEN_DENSITY := 360
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/nabu_defconfig
+
+# Inherit from the proprietary version
+include vendor/xiaomi/nabu/BoardConfigVendor.mk
---
+++ lineage_nabu.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit from nabu device
+$(call inherit-product, device/xiaomi/nabu/device.mk)
+
+PRODUCT_NAME := lineage_nabu
+PRODUCT_DEVICE := nabu
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi Pad 5
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="nabu_global-user 13 RKQ1.200826.002 V14.0.5.0.TKXMIXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/nabu_global/nabu:13/RKQ1.200826.002/V14.0.5.0.TKXMIXM:user/release-keys
